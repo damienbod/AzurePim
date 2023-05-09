c693e6edbc359a87a549e5c61cc8f973f6a8407f
damienbod <damien_bod@hotmail.com>
2023-05-09 21:11:46 +0200

    code clean up

--- AzurePim/Program.cs
+++ AzurePim/Program.cs
@@ -19,7 +19,6 @@ public class Program
 
         builder.Services.AddAuthorization(options =>
         {
-            // By default, all incoming requests will be authorized according to the default policy.
             options.FallbackPolicy = options.DefaultPolicy;
         });
         builder.Services.AddRazorPages()
@@ -27,11 +26,9 @@ public class Program
 
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
         if (!app.Environment.IsDevelopment())
         {
             app.UseExceptionHandler("/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             app.UseHsts();
         }
 
